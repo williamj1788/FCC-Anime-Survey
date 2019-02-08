7c42207236b0ef00d3d33a45dbec771523398195
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-02-08 05:50:25 -0500

    Add whiteSpace to improve readability

--- Server.js
+++ Server.js
@@ -3,28 +3,29 @@ var url = require('url');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
+  
   res.setHeader('Access-Control-Allow-Origin','*');
   res.writeHead(200, {'Content-Type': 'text/plain'});
+ 
   var q = url.parse(req.url,true);
-  console.log(q);
   var data = q.query;
+  
   if(data.name){
     fs.readFile('responses.json', function(err,content){
+      
       var parseJson = JSON.parse(content);
-      x = content;
       parseJson.responses.push(data);
+      
       fs.writeFile('responses.json', JSON.stringify(parseJson,null,2), function (err) {
         if (err) throw err;
         console.log("Fin");
       });
-      res.end(x);
+      
+      res.end(content);
     })
   }else{
     console.log("No Data!")
   }
  
-  
-  
-  
 }).listen(8080);
 
